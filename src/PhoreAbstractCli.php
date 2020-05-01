4a2bc815423b5b15095d48249f7a670cf3eddc93
Matthes <matthes@leuffen.de>
2020-05-01 13:12:07 +0200

    update

--- src/PhoreAbstractCli.php
+++ src/PhoreAbstractCli.php
@@ -227,9 +227,15 @@ abstract class PhoreAbstractCli
         } catch (UserInputException $e) {
             $this->emergency(  ColorOutput::Str($e->getMessage(), "red"). PHP_EOL);
             $this->emergency(  "Run '$this->cmdName -h' to see help". PHP_EOL);
+
+            if ($this->outMode === 2)
+                throw $e; // Throw exception to main if debug mode (see backtrace)
             exit(1);
         } catch (\Exception $e) {
             $this->emergency(ColorOutput::Str("Error: " . $e->getMessage(), "red"). PHP_EOL);
+            $this->emergency("Set parameter '--verbose' to see the backtrace" . PHP_EOL);
+            if ($this->outMode === 2)
+                throw $e; // Throw exception to main if debug mode (see backtrace)
             exit(1);
         }
     }
